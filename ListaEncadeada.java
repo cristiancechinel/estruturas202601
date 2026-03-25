7cd59bc3c46f1fdcd922e2d3f27bf200503ca6f7
cristiancechinel <contato@cristiancechinel.pro.br>
2026-03-25 20:03:28 -0300

    25.03.2026

--- ListaEncadeada.java
+++ ListaEncadeada.java
@@ -6,7 +6,7 @@ public class ListaEncadeada {
     private Nodo inicio;
     
     private class Nodo{
-        int dado;
+        Integer dado;
         Nodo prox; 
     }
 
@@ -20,11 +20,46 @@ public class ListaEncadeada {
         novo.prox = inicio; 
         inicio = novo;
     }
+    
+    Integer removeInicio(){
+        if (inicio != null){
+            Nodo temp = inicio;
+            inicio = inicio.prox;
+            return temp.dado;
+        }
+        return null;
+    }
+    
+    Integer removeNodo(int n){
+        Nodo temp = inicio;
+        Nodo ant = null;
+    
+        while (temp != null && temp.dado !=n){
+            ant = temp;
+            temp = temp.prox;
+        }
+    
+        if (temp == null) return null; //elemento não existe
+    
+        if (ant == null){ //encontrou no inicio
+            int retira = inicio.dado;
+            inicio = inicio.prox;
+            return retira;
+        }
+ 
+        int retira = temp.dado;
+        ant.prox = temp.prox;
+        return retira;
+    }
+    
+    
+    
    
     void imprime(){
             for (Nodo temp = inicio; temp != null; temp = temp.prox){
                  System.out.print(temp.dado + "->");
             }
+            System.out.println();
     
     }
 }
--- TestaLista.java
+++ TestaLista.java
@@ -12,9 +12,13 @@ public class TestaLista {
         lista.insereInicio(15);
         lista.insereInicio(26);
         lista.insereInicio(55);
-        
+                
         lista.imprime();
     
+        System.out.println("removeu " + lista.removeNodo(15));
+        
+        
+        lista.imprime();
     
     
     }
